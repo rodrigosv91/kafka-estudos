677f18311e632be780cb07ab093cfb6b44eae71e
rodrigosv91 <rodrigosv91@gmail.com>
2023-05-19 10:58:24 -0300

    refactor: removido import desnecessario em NewOrderMain

--- src/main/db/kafka/ecommerce/NewOrderMain2.java
+++ src/main/db/kafka/ecommerce/NewOrderMain2.java
@@ -1,9 +1,6 @@
 package db.kafka.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.math.BigDecimal;
@@ -30,11 +27,4 @@ public class NewOrderMain2 {
         }
     }
 
-    private static Properties properties(){
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
-    }
 }
